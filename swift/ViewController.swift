988792d482c94ffd3c0b6aa83f24c7d7ff5af862
muhammed malallah <vec.xyz.tor@gmail.com>
2020-01-04 22:36:34 +0300

    Initial Commit

---
+++ swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by Muhammed Najim on 1/4/20.
+//  Copyright © 2020 Muhammed Najim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
